a015d997bde1620839fd0d3da4c6edcb7b8f5ed9
Alex Constantinou <alexconstantinou@Alexs-MBP.lan>
2018-12-09 19:21:54 +0000

    persist login route added

--- src/main/java/com/myteacher/Student/Controllers/StudentController.java
+++ src/main/java/com/myteacher/Student/Controllers/StudentController.java
@@ -3,10 +3,16 @@ package com.myteacher.Student.Controllers;
 import com.myteacher.Student.Models.Student;
 import com.myteacher.Student.Repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.myteacher.Student.Security.SecurityConstants.HEADER_STRING;
+import static com.myteacher.Student.Security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/")
@@ -18,6 +24,13 @@ public class StudentController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
+    }
+
+    @RequestMapping(value = "/persist-login", method = RequestMethod.GET)
+    public ResponseEntity<String> handle(@CookieValue("studentToken") String studentToken){
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set(HEADER_STRING, TOKEN_PREFIX + studentToken);
+        return new ResponseEntity<String>(responseHeaders, HttpStatus.CREATED);
     }
 
     @PreAuthorize("hasRole('STUDENT')")
